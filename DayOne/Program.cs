320c3ec181a33ae8c50b976829ab397a54bfeff6
Shanna <shanna.vangrevengoed@thelearningnetwork.nl>
2021-11-30 15:20:46 +0100

    first commit

---
+++ DayOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayOne
+{
+    class Program
+    {
+        public void AnswerDayOne()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+    }
+}
